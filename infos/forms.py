c7f76fa93471198ffe45f941766b1f6db5608c27
pedroCarvalhoCintra <pedrocintra2@gmail.com>
2023-09-16 17:46:26 -0300

    Create report form logic (tests)

--- infos/forms.py
+++ infos/forms.py
@@ -1,9 +1,10 @@
+
 from django import forms
 
 
 class ReportForm(forms.Form):
-    company_name = forms.CharField(label="Company", max_length=100)
+    company_id = forms.IntegerField(label="Company")
     link = forms.URLField(label="Link")
     description = forms.CharField(label="Description", max_length=300)
-    email = forms.EmailField(label="Email")
+    # email = forms.EmailField(label="Email")
     contact_permission = forms.BooleanField()
--- infos/views.py
+++ infos/views.py
@@ -1,3 +1,5 @@
+from .models import Report
+
 from django.shortcuts import render
 from django.views.generic.edit import FormView
 from django.views.generic import TemplateView
@@ -12,3 +14,20 @@ class ReportSucessView(TemplateView):
 class ReportFormView(FormView):
     template_name = "report.html"
     success_url = "/report/success/"
+
+    def form_valid(self, form):
+        data = form.cleaned_data
+        report = Report(
+            "titulo",
+            data["description"],
+            "categoria 1", #categoryId
+            data["link"], 
+            data["company_id"], 
+            #userId
+            "created_at",
+            "updated_at"
+        )
+
+        report.save()
+
+        return super().form_valid(form)
