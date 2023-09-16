8036b4c95f4910ee1d69be597db91ef1198f3118
pedroCarvalhoCintra <pedrocintra2@gmail.com>
2023-09-16 15:48:48 -0300

    Created the ReportForm
    Added a class to obtain the report form in infos/forms.py

--- infos/forms.py
+++ infos/forms.py
@@ -2,4 +2,8 @@ from django import forms
 
 
 class ReportForm(forms.Form):
-    pass
+    company_name = forms.CharField(label="Company", max_length=100)
+    link = forms.URLField(label="Link")
+    description = forms.CharField(label="Description", max_length=300)
+    email = forms.EmailField(label="Email")
+    contact_permission = forms.BooleanField()
