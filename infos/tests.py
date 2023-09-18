ea5305ba2f108c2a5f90b756e832b5e525aa2e5c
ra247362 <r247362@dac.unicamp.br>
2023-09-17 21:25:10 -0300

    refactor(reportform): finish tests.py

--- infos/tests.py
+++ infos/tests.py
@@ -1,54 +1,83 @@
-from django.test import TestCase
-from django.test import Client
-
-from .models import Report, Company, ReportCategory
-from .forms import ReportForm
-from django.utils import timezone
 from django.contrib.auth import get_user
+from django.test import Client, RequestFactory, TestCase
+from django.urls import reverse
+from django.utils import timezone
+
+from .forms import ReportForm
+from .models import Company, Report, ReportCategory
+from .views import ReportFormView
+
+factory = RequestFactory()
 
 
 class ReportTestCase(TestCase):
-    
     def setUp(self) -> None:
-        Company.objects.create(name="borin")
-        ReportCategory.objects.create(name="borin",formatted_name="BORIN")
+        Company.objects.create(name="teste")
+        ReportCategory.objects.create(name="teste", formatted_name="TESTE")
         self.current_date = timezone.now().isoformat(timespec="seconds")
-        self.company = Company.objects.get(name="borin")
-        self.category = ReportCategory.objects.get(name="borin")
-
+        self.company = Company.objects.get(name="teste")
+        self.category = ReportCategory.objects.get(name="teste")
 
     def test_report_request(self):
         client = Client()
-        data = {"company": self.company, "category": self.category, "link": "https://borin.com", "description": "borin borin", "contact_permission": True}
-        response = client.post("http://localhost:8000/info/report/", data=data)
+        data = {
+            "company": self.company,
+            "category": self.category,
+            "link": "https://teste.com",
+            "description": "teste teste",
+            "contact_permission": True,
+        }
+        response = client.post("/info/report/", data=data)
         self.assertEqual(response.status_code, 200)
 
     def test_validation_company_in_form(self):
-        form = ReportForm(data= {"company": "Empresa borin", "category": self.category, "link": "https://borin.com", "description": "borin borin", "contact_permission": True})
+        form = ReportForm(
+            data={
+                "company": "Empresa teste",
+                "category": self.category,
+                "link": "https://teste.com",
+                "description": "teste teste",
+                "contact_permission": True,
+            }
+        )
         form.is_valid()
-        self.assertEquals(list(form.errors.keys())[0], 'company')
+        self.assertEquals(list(form.errors.keys())[0], "company")
 
     def test_validation_category_in_form(self):
-        form = ReportForm(data= {"company": self.company, "category": "categoria1", "link": "https://borin.com", "description": "borin borin", "contact_permission": True})
+        form = ReportForm(
+            data={
+                "company": self.company,
+                "category": "categoria1",
+                "link": "https://teste.com",
+                "description": "teste teste",
+                "contact_permission": True,
+            }
+        )
         form.is_valid()
-        self.assertEquals(list(form.errors.keys())[0], 'category')
+        self.assertEquals(list(form.errors.keys())[0], "category")
 
     def test_validation_link_in_form(self):
-        form = ReportForm(data= {"company": self.company, "category": self.category, "link": "https://a", "description": "borin borin", "contact_permission": True})
+        form = ReportForm(
+            data={
+                "company": self.company,
+                "category": self.category,
+                "link": "https://a",
+                "description": "teste teste",
+                "contact_permission": True,
+            }
+        )
         form.is_valid()
-        self.assertEquals(list(form.errors.keys())[0], 'link')
+        self.assertEquals(list(form.errors.keys())[0], "link")
 
-    # def test_validation_description_in_form(self):
-    #     form = ReportForm(data= {"company": self.company, "category": self.category, "link": "https://borin.com", "description": "", "contact_permission": True})
-    #     form.is_valid()
-    #     self.assertEquals(list(form.errors.keys())[0], 'description')
-
-
-    # def test_reports_being_save(self):
-    #     client = Client()
-    #     data = {"company": self.company, "category": self.category, "link": "https://borin.com", "description": "borin borin", "contact_permission": True}
-    #     client.post("http://localhost:8000/info/report/", data=data)
-
-    #     report = Report.objects.all()
-    #     print(report)
-    #     # self.assertEquals(report.title, f"{self.current_date}-{data['company'].name}-{data['category'].name}")
+    def test_validation_description_in_form(self):
+        form = ReportForm(
+            data={
+                "company": self.company,
+                "category": self.category,
+                "link": "https://teste.com",
+                "description": "",
+                "contact_permission": True,
+            }
+        )
+        form.is_valid()
+        self.assertEquals(list(form.errors.keys())[0], "description")
