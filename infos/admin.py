e7bbd34bcb9faeedc621952a7a1063b86bd8adb1
Lozavival <lozavival@gmail.com>
2023-11-18 19:17:21 -0300

    feat(info): create lawsuit and news models

--- infos/admin.py
+++ infos/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Report, ReportCategory
+from .models import Lawsuit, News, Report, ReportCategory
 
 admin.site.register(Report)
 admin.site.register(ReportCategory)
+admin.site.register(Lawsuit)
+admin.site.register(News)
--- infos/models.py
+++ infos/models.py
@@ -1,18 +1,26 @@
 from django.db import models
 
 
-class Report(models.Model):
+class InfoBase(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    category = models.ForeignKey("ReportCategory", on_delete=models.SET_NULL, null=True)
-    links = models.TextField()
-    company = models.ForeignKey("company.Company", on_delete=models.CASCADE)
-    user = models.ForeignKey("user.User", on_delete=models.SET_NULL, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        abstract = True
+
+
+class Report(InfoBase):
+    company = models.ForeignKey(
+        "company.Company", on_delete=models.CASCADE, related_name="reports"
+    )
+    category = models.ForeignKey("ReportCategory", on_delete=models.SET_NULL, null=True)
+    links = models.TextField()
+    user = models.ForeignKey("user.User", on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class ReportCategory(models.Model):
@@ -26,3 +34,23 @@ class ReportCategory(models.Model):
 
     class Meta:
         verbose_name_plural = "Report categories"
+
+
+class Lawsuit(InfoBase):
+    company = models.ForeignKey(
+        "company.Company", on_delete=models.CASCADE, related_name="lawsuits"
+    )
+    source = models.URLField()
+    start_year = models.PositiveSmallIntegerField()
+    resolution_year = models.PositiveSmallIntegerField(null=True, blank=True)
+
+
+class News(InfoBase):
+    company = models.ForeignKey(
+        "company.Company", on_delete=models.CASCADE, related_name="news"
+    )
+    author = models.CharField(max_length=50)
+    date = models.DateField()
+
+    class Meta:
+        verbose_name_plural = "News"
