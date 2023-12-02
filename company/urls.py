8b3bcc93033bacd4eb6599e3cab835dbf0bc0887
Lozavival <lozavival@gmail.com>
2023-12-02 17:17:05 -0300

    feat(lawsuit): create lawsuit list page
    Create view and URL to lawsuit list and link to lawsuit list on company page

--- company/urls.py
+++ company/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .views import CompanyView, ExplorerView, NewsList, ReportsList
+from .views import CompanyView, ExplorerView, LawsuitsList, NewsList, ReportsList
 
 app_name = "company"
 urlpatterns = [
     path("", ExplorerView.as_view(), name="explorer"),
     path("<int:company_id>/", CompanyView.as_view(), name="company"),
-    path('<int:company_id>/noticias', NewsList.as_view(), name="news"),
-    # path('<int:company_id>/processos/',LawsuitsView.as_view(), name="lawsuits"),
+    path("<int:company_id>/noticias", NewsList.as_view(), name="news"),
+    path("<int:company_id>/processos", LawsuitsList.as_view(), name="lawsuits"),
     path("<int:company_id>/denuncias", ReportsList.as_view(), name="reports"),
 ]
