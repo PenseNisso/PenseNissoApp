3accac1ad51cbd5bd6bf7e53c71edf0a608c24d0
DuduRPP <e247055@dac.unicamp.br>
2023-12-05 10:06:11 -0300

    feat: made possible to add companies to comparator

--- comparator/views.py
+++ comparator/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.views import View
 
-# Create your views here.
+from company.models import Company
 
 class ComparatorView(View):
     def get(self, request):
         request.session["name"] = "comparator"
-        context = {"name": request.session["name"]}
+        request.session["selected_companies"] = []
+        context = {"name": request.session["name"],
+                   "companies": Company.objects.all(),
+                   "selected_companies": request.session["selected_companies"]
+                  }
+        return render(request, "comparator.html", context)
+    def post(self, request):
+        request.session["selected_companies"].append(request.POST.get("company"))
+        request.session.modified = True
+        #request.session["selected_companies"].append(Company.objects.filter(name = request.POST.get("company")))
+        selected_query = Company.objects.filter(name__in = request.session["selected_companies"])
+        print(selected_query)
+        print(request.session["selected_companies"])
+        context = {"name": request.session["name"],
+                   "companies": Company.objects.exclude(name__in = request.session["selected_companies"]),
+                   "selected_companies": selected_query
+                  }
         return render(request, "comparator.html", context)
