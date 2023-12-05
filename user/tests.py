ea203bab136571c9553b8106fd19359161379e2a
ra247362 <r247362@dac.unicamp.br>
2023-12-05 16:28:00 -0300

    refactor(tests): added visible test results to tests
    Added print statements to ensure test results are visible while running unit tests.

--- user/tests.py
+++ user/tests.py
@@ -12,6 +12,7 @@ class UserModelTest(TestCase):
 
     def test_user_str(self):
         self.assertEqual(str(self.user), "Test_User")
+        print("Teste User-Model-1: Usuário criado com sucesso.")
 
 
 class ProfileViewTest(TestCase):
@@ -24,43 +25,54 @@ class ProfileViewTest(TestCase):
         self.client.login(username="Test_User", password="testpassword")
         response = self.client.get(reverse("user:profile", args=[self.user.id]))
         self.assertEqual(response.status_code, 200)
+        print("Teste User-ProfileView-1: Login realizado com sucesso (Cód 200).")
 
     def test_profile_view_template(self):
         self.client.login(username="Test_User", password="testpassword")
         response = self.client.get(reverse("user:profile", args=[self.user.id]))
         self.assertTemplateUsed(response, "profile.html")
+        print("Teste User-ProfileView-2: Template aplicado com sucesso.")
 
     def test_profile_view_context(self):
         self.client.login(username="Test_User", password="testpassword")
         response = self.client.get(reverse("user:profile", args=[self.user.id]))
         self.assertEqual(response.context["object"].username, "Test_User")
+        print(
+            "Teste User-ProfileView-3: Variáveis de contexto verificadas com sucesso."
+        )
 
 
 class RegisterViewTest(TestCase):
     def test_register_view_status_code(self):
         response = self.client.get(reverse("user:register"))
         self.assertEqual(response.status_code, 200)
+        print("Teste User-RegisterView-1: Acesso realizado com sucesso (Cód 200).")
 
     def test_register_view_template(self):
         response = self.client.get(reverse("user:register"))
         self.assertTemplateUsed(response, "register.html")
+        print("Teste User-RegisterView-2: Template usado com sucesso.")
 
 
 class LoginViewTest(TestCase):
     def test_login_view_status_code(self):
         response = self.client.get(reverse("user:login"))
         self.assertEqual(response.status_code, 200)
+        print("Teste User-LoginView-1: Acesso realizado com sucesso (Cód 200).")
 
     def test_login_view_template(self):
         response = self.client.get(reverse("user:login"))
         self.assertTemplateUsed(response, "login.html")
+        print("Teste User-LoginView-2: Template usado com sucesso.")
 
 
 class LogoutViewTest(TestCase):
     def test_logout_view_status_code(self):
         response = self.client.get(reverse("user:logout"))
         self.assertEqual(response.status_code, 200)
+        print("Teste User-LogoutView-1: Acesso realizado com sucesso (Cód 200).")
 
     def test_logout_view_template(self):
         response = self.client.get(reverse("user:logout"))
         self.assertTemplateUsed(response, "logout.html")
+        print("Teste User-LoginView-2: Template usado com sucesso.")
