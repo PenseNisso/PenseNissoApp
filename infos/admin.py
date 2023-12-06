261fb4aa9544207afe8b35ccdd55a759fdf60091
Lozavival <lozavival@gmail.com>
2023-12-06 18:30:37 -0300

    refactor(admin): add status to report admin list
    Add status column on report list and filter by status option

--- infos/admin.py
+++ infos/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 
 from .models import Lawsuit, News, Report, ReportCategory
 
-admin.site.register(Report)
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ("title", "status")
+    list_filter = ("status",)
+
+
 admin.site.register(ReportCategory)
 admin.site.register(News)
 admin.site.register(Lawsuit)
