da603799962719ba0e391ffd097700bf8bc863d7
Lozavival <lozavival@gmail.com>
2023-12-06 13:29:42 -0300

    feat(moderation): create moderator group
    Create Moderator group and add report permissions.

--- user/apps.py
+++ user/apps.py
@@ -4,3 +4,15 @@ from django.apps import AppConfig
 class UserConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "user"
+
+    def ready(self) -> None:
+        from django.contrib.auth.models import Group, Permission
+        from django.contrib.contenttypes.models import ContentType
+
+        from infos.models import Report
+
+        moderator_group, created = Group.objects.get_or_create(name="Moderator")
+        content_type = ContentType.objects.get_for_model(Report)
+        permissions = Permission.objects.filter(content_type=content_type)
+        for perm in permissions:
+            moderator_group.permissions.add(perm)
