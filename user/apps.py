1a8d5d9d6a2d69ecfd53f753b22c79396a25cae9
Lozavival <lozavival@gmail.com>
2023-12-06 19:25:36 -0300

    fix(moderation): fix moderator group creation
    Surround group creation with try-except block to prevent error on db creation

--- user/apps.py
+++ user/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.db.utils import OperationalError
 
 
 class UserConfig(AppConfig):
@@ -6,13 +7,16 @@ class UserConfig(AppConfig):
     name = "user"
 
     def ready(self) -> None:
-        from django.contrib.auth.models import Group, Permission
-        from django.contrib.contenttypes.models import ContentType
+        try:
+            from django.contrib.auth.models import Group, Permission
+            from django.contrib.contenttypes.models import ContentType
 
-        from infos.models import Report
+            from infos.models import Report
 
-        moderator_group, created = Group.objects.get_or_create(name="Moderator")
-        content_type = ContentType.objects.get_for_model(Report)
-        permissions = Permission.objects.filter(content_type=content_type)
-        for perm in permissions:
-            moderator_group.permissions.add(perm)
+            moderator_group, created = Group.objects.get_or_create(name="Moderator")
+            content_type = ContentType.objects.get_for_model(Report)
+            permissions = Permission.objects.filter(content_type=content_type)
+            for perm in permissions:
+                moderator_group.permissions.add(perm)
+        except OperationalError:
+            pass
