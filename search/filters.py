ceba28cb432511a630bf3f386cafb64e4670aa92
ra247362 <r247362@dac.unicamp.br>
2023-12-07 10:29:44 -0300

    fix(search/filter): fix minor bug
    Fixed mistake in previous commit.

--- search/filters.py
+++ search/filters.py
@@ -2,6 +2,7 @@ from django.db.models import Q
 
 from infos.models import Lawsuit, Report, News
 
+
 class AbstractFilter:
     def get_q(self):
         pass
@@ -11,11 +12,17 @@ class BooleanFilter(AbstractFilter):
     def __init__(self, value, type) -> None:
         self.type = type
         self.value = value
+        if type == "report":
+            pass
+        elif type == "news":
+            pass
+        elif type == "lawsuits":
+            pass
 
     def get_q(self) -> Q:
         query = ""
         if self.value == "yes":
-            q = Q()
+            q = Q(pk__in=[report.company.id for report in Report.objects.all()])
         elif self.value == "no":
             q = Q()
         else:
@@ -27,6 +34,7 @@ class TextFilter(AbstractFilter):
     def __init__(self, value, type) -> None:
         self.type = type
         self.value = value
+
     def get_q(self) -> Q:
         type = self.type
-        return Q(**{type+"__icontains":self.value})
+        return Q(**{type + "__icontains": self.value})
