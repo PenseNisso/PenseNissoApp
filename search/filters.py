ac349f23738fe4bb42c28aa007f88488c0e54fa7
ra247362 <r247362@dac.unicamp.br>
2023-12-08 12:09:21 -0300

    refactor(search/filters): extract method
    Fixed code smell in filters.py.

--- search/filters.py
+++ search/filters.py
@@ -1,24 +1,18 @@
 from django.db.models import Q
 
-from infos.models import Lawsuit, Report, News
+from infos.models import Lawsuit, News, Report
 
 
 class AbstractFilter:
+    def __init__(self, value, type) -> None:
+        self.type = type
+        self.value = value
+
     def get_q(self):
         pass
 
 
 class BooleanFilter(AbstractFilter):
-    def __init__(self, value, type) -> None:
-        self.type = type
-        self.value = value
-        if type == "report":
-            pass
-        elif type == "news":
-            pass
-        elif type == "lawsuits":
-            pass
-
     def get_q(self) -> Q:
         type = self.type
         if self.value == "yes":
@@ -31,10 +25,6 @@ class BooleanFilter(AbstractFilter):
 
 
 class TextFilter(AbstractFilter):
-    def __init__(self, value, type) -> None:
-        self.type = type
-        self.value = value
-
     def get_q(self) -> Q:
         type = self.type
         return Q(**{type + "__icontains": self.value})
