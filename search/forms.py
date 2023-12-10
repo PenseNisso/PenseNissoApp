7f89c6ccbf6b95e4c36645c57e7692ed874849d1
ra247362 <r247362@dac.unicamp.br>
2023-12-10 09:10:10 -0300

    feature(search/sorting): added basic sorting functionality to explorer
    Adds sorting options to explorer. System should allow for easy expansion in the future.

--- search/forms.py
+++ search/forms.py
@@ -23,3 +23,19 @@ class FilterForm(forms.Form):
         choices=choices_has_info,
         initial=choices_has_info[0],
     )
+
+
+class SortingForm(forms.Form):
+    sorting_options = [
+        ("alphabetical_ascending", "A-Z"),
+        ("alphabetical_descending", "Z-A"),
+        ("most_reports", "Mais Denúncias"),
+        ("least_reports", "Menos Denúncias"),
+    ]
+
+    sorting = forms.ChoiceField(
+        label="Ordenar por:",
+        required=False,
+        choices=sorting_options,
+        initial=sorting_options[0],
+    )
