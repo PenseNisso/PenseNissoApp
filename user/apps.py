aa77832f7e4c6daf185521da3e1fa24501f1536b
Lozavival <lozavival@gmail.com>
2023-12-13 00:01:12 -0300

    feat(moderation): give company permissions to moderator group

--- user/apps.py
+++ user/apps.py
@@ -11,11 +11,12 @@ class UserConfig(AppConfig):
             from django.contrib.auth.models import Group, Permission
             from django.contrib.contenttypes.models import ContentType
 
+            from company.models import Company
             from infos.models import Report
 
             moderator_group, created = Group.objects.get_or_create(name="Moderator")
-            content_type = ContentType.objects.get_for_model(Report)
-            permissions = Permission.objects.filter(content_type=content_type)
+            content_types = ContentType.objects.get_for_models(Report, Company).values()
+            permissions = Permission.objects.filter(content_type__in=content_types)
             for perm in permissions:
                 moderator_group.permissions.add(perm)
         except OperationalError:
