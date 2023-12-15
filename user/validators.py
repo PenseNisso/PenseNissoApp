9e08a7e47fb6d9555cd57a4e6e8695177f97b10d
Lozavival <lozavival@gmail.com>
2023-12-15 17:38:00 -0300

    feat(user): modify password validators

---
+++ user/validators.py
@@ -0,0 +1,78 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
+
+
+class LengthValidator:
+    def __init__(self, min_length=8, max_length=50):
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def validate(self, password, user=None):
+        size = len(password)
+        if size < self.min_length:
+            raise ValidationError(
+                _("This password must contain at least %(min_length)d characters."),
+                code="password_too_short",
+                params={"min_length": self.min_length},
+            )
+        if size > self.max_length:
+            raise ValidationError(
+                _("This password must contain at most %(max_length)d characters."),
+                code="password_too_long",
+                params={"max_length": self.max_length},
+            )
+
+    def get_help_text(self):
+        return _(
+            f"Sua senha deve conter entre {self.min_length} e {self.max_length} caracteres."
+        )
+
+
+class CaseValidator:
+    def validate(self, password: str, user=None):
+        uppercase = [ch.isupper() for ch in password]
+        if not any(uppercase):
+            raise ValidationError(
+                _("Essa senha não contém nenhuma letra maiúscula."),
+                code="password_without_uppercase",
+            )
+        if all(uppercase):
+            raise ValidationError(
+                _("Essa senha não contém nenhuma letra minúscula."),
+                code="password_without_lowercase",
+            )
+
+    def get_help_text(self):
+        return _("Sua senha precisa conter letras maiúsculas e minúsculas.")
+
+
+class NumericValidator:
+    def validate(self, password: str, user=None):
+        for ch in password:
+            if ch.isnumeric():
+                return
+        raise ValidationError(
+            _("Essa senha não contém números."),
+            code="password_without_number",
+        )
+
+    def get_help_text(self):
+        return _("Sua senha precisa conter números.")
+
+
+class SpecialCharacterValidator:
+    special_characters = "'\"!@#$%&*-_=+;:?"
+
+    def validate(self, password: str, user=None):
+        for ch in password:
+            if ch in self.special_characters:
+                return
+        raise ValidationError(
+            _("Essa senha não contém caracteres especiais."),
+            code="password_without_special_characters",
+        )
+
+    def get_help_text(self):
+        return _(
+            f"Sua senha precisa conter caracteres especiais ({'/'.join(self.special_characters)})."
+        )
